4537f07f10e3d36e9b5797d7e41cf245400d3fbe
essgeejay <justincredible75@gmail.com>
2021-03-03 16:28:34 +0000

    finsihed logic

--- Dom Version/app.js
+++ Dom Version/app.js
@@ -1,9 +1,21 @@
 let tieScore = 0;
 let playerScore = 0;
 let computerScore = 0;
-const rock_btn = document.getElementById("btnRock")
-const paper_btn = document.getElementById("btnPaper")
-const scissors_btn = document.getElementById("btnScissors")
+
+// these variables are dom elements to playerScore and computerScore above!
+const playscore_span = document.getElementById("userscore");
+const computerscore_span = document.getElementById("compscore");
+
+// not using getElementbyID - instead were using a *querySelector because were calling a class. using the dot notation. 
+const scoreboard_div = document.getElementsByClassName('.keepscore');
+
+// this should change the static text into dynamic text
+const results_p = document.querySelector('.result > p');
+
+const rock_btn = document.getElementById("btnRock");
+const paper_btn = document.getElementById("btnPaper");
+const scissors_btn = document.getElementById("btnScissors");
+const buttons = document.querySelectorAll('button');
 
 
 // A function that randomises the choice for the computer. 
@@ -12,53 +24,87 @@ function computerPlay() {
     return choices[Math.floor(Math.random() * choices.length)];
 }
 
+function disabledbuttons() {
+
+    buttons.forEach((button) => {
+        button.disabled = true
+
+    });
+
+}
+
+
+
+function lose() {
+    computerScore++
+    computerscore_span.innerHTML = computerScore;
+    if (computerScore === 5) {
+        console.log("computer wins")
+        disabledbuttons();
+    };
+    // document.getElementById(playerSelection).classList.add("green-glow");
+};
+
+function win() {
+    playerScore++
+    playscore_span.innerHTML = playerScore;
+    if (playerScore === 5) {
+        console.log("thats a wrap!")
+        disabledbuttons();
+    };
+}
+
+function tie() {
+    computerscore_span.innerHTML = computerScore;
+    playscore_span.innerHTML = playerScore;
+    // if (playerScore === computerScore) {
+    //     console.log("Nobody won!")
+    //     disabledbuttons();
+    // };
+};
+
 
 function playRound(playerSelection) {
 
     const computerSelection = computerPlay();
-
     // You lose...
     if (playerSelection === 'r' && computerSelection === 'p') {
-        computerScore++
-        return console.log("YOU LOSE! - Paper beats Rock!");
+        lose();
+        results_p.innerHTML = "YOU LOSE! - Paper beats Rock!";
     }
     else if (playerSelection === 'p' && computerSelection === 's') {
-        computerScore++
-        return console.log("YOU LOSE! - scissors beats paper!");
+        lose();
+        results_p.innerHTML = "YOU LOSE! - Scissors beats Paper!";
     }
     else if (playerSelection === 's' && computerSelection === 'r') {
-        computerScore++
-        return console.log("YOU LOSE! - rock beats scissors!");
+        lose();
+        results_p.innerHTML = "YOU LOSE! - Rock beats Scissors!";
     }
     // You win...
     else if (playerSelection === 'r' && computerSelection === 's') {
-        playerScore++
-        return console.log("YOU WIN! - rock beats scissors!");
+        win();
+        results_p.innerHTML = "YOU WIN! - Rock beats Scissors!";
     }
     else if (playerSelection === 'p' && computerSelection === 'r') {
-        playerScore++
-        return console.log("YOU WIN! - Paper beats Rock!");
+        win();
+        results_p.innerHTML = "YOU WIN! - Paper beats Rock!";
     }
     else if (playerSelection === 's' && computerSelection === 'p') {
-        playerScore++
-        return console.log("YOU WIN! - Scissors beats Paper!");
+        win();
+        results_p.innerHTML = "YOU WIN! - Scissors beats Paper!";
     }
     // You both tie...
     else {
-        tieScore++
-        return console.log("Both tie!!")
-    }
+        tie();
+        results_p.innerHTML = "Its a DRAW!"
+    };
 
 }
 
 
 function run_game() {
-    // this controls the button clicks
-    rock_btn.addEventListener("click", function () {
-        playRound("r");
-        console.log("you picked Rock")
-
-    })
+    // this controls the button clicks - using es6 methods
+    rock_btn.addEventListener('click', () => playRound("r"));
 
     paper_btn.addEventListener("click", function () {
         playRound("p");
@@ -72,7 +118,31 @@ function run_game() {
 };
 
 
+
+// while result() {
+//     if (playerScore >= computerScore) {
+//         console.log("Nobody wins!")
+//     }
+//     else if (playerScore > computerScore) {
+//         results_p.innerHTML = "you win the game!";
+//     }
+//     else {
+//         return "You lose the game!! hahahaha"
+//     };
+// }
+
+
+
+// for (let count = 0; count <= 4; count++) {
+
+
+
+
+// }
+
 run_game();
+
+
 
 
 
